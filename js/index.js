2fa494ad12dfd2d26833a43861ea35d99aeb05b7
Christopher Wright <christopherwright@Christophers-MacBook-Pro.local>
2018-06-12 14:43:45 -0400

    js solution for expander margin-left shifting

--- js/index.js
+++ js/index.js
@@ -1,49 +1,102 @@
-(function() {
-  var carousel, next, prev, seats;
+// (function() {
+//   var carousel, next, prev, seats;
 
-  carousel = $('.carousel');
+//   carousel = $('.carousel');
 
-  seats = $('.carousel-seat');
+//   seats = $('.carousel-seat');
 
-  if(seats.length)
+//   if(seats.length)
 
-  next = function(el) {
-    if (el.next().length > 0) {
-      return el.next();
-    } else {
-      return seats.first();
+//   next = function(el) {
+//     if (el.next().length > 0) {
+//       return el.next();
+//     } else {
+//       return seats.first();
+//     }
+//   };
+
+//   prev = function(el) {
+//     if (el.prev().length > 0) {
+//       return el.prev();
+//     } else {
+//       return seats.last();
+//     }
+//   };
+
+//   $('.toggle').on('click', function(e) {
+//     var el, i, j, new_seat, ref;
+//     el = $('.is-ref').removeClass('is-ref');
+//     if ($(e.currentTarget).data('toggle') === 'next') {
+//       new_seat = next(el);
+//       carousel.removeClass('is-reversing');
+//     } else {
+//       new_seat = prev(el);
+//       carousel.addClass('is-reversing');
+//     }
+//     new_seat.addClass('is-ref').css('order', 1);
+//     for (i = j = 2, ref = seats.length; (2 <= ref ? j <= ref : j >= ref); i = 2 <= ref ? ++j : --j) {
+//       new_seat = next(new_seat).css('order', i);
+//     }
+//     carousel.removeClass('is-set');
+//     return setTimeout((function() {
+//       return carousel.addClass('is-set');
+//     }), 50);
+//   });
+
+// }).call(this);
+
+
+var timesRight = 0;
+
+$('.toggle.left').on('click', function(){
+
+if (timesRight > 0){
+
+  if(timesRight === 1){
+      $( ".carousel" ).animate({ left: 0 })
+      --timesRight; 
+
+    }else if (timesRight === 2){
+      $( ".carousel" ).animate({ left: - $('.carousel-seat').width() * 2 })
+      --timesRight; 
+
     }
-  };
 
-  prev = function(el) {
-    if (el.prev().length > 0) {
-      return el.prev();
-    } else {
-      return seats.last();
-    }
-  };
+  }
+  
 
-  $('.toggle').on('click', function(e) {
-    var el, i, j, new_seat, ref;
-    el = $('.is-ref').removeClass('is-ref');
-    if ($(e.currentTarget).data('toggle') === 'next') {
-      new_seat = next(el);
-      carousel.removeClass('is-reversing');
-    } else {
-      new_seat = prev(el);
-      carousel.addClass('is-reversing');
-    }
-    new_seat.addClass('is-ref').css('order', 1);
-    for (i = j = 2, ref = seats.length; (2 <= ref ? j <= ref : j >= ref); i = 2 <= ref ? ++j : --j) {
-      new_seat = next(new_seat).css('order', i);
-    }
-    carousel.removeClass('is-set');
-    return setTimeout((function() {
-      return carousel.addClass('is-set');
-    }), 50);
-  });
+});
 
-}).call(this);
+$('.toggle.right').on('click', function(){
+  if(timesRight === 0){
+     $( ".carousel" ).animate({ left: - $('.carousel-seat').width() * 2 })
+    ++timesRight; 
+
+  }else if( timesRight === 1 ){
+    var wrap = document.querySelector('.wrap');
+    console.log(wrap.clientWidth);
+    if(wrap.clientWidth <= 602){
+        $( ".carousel" ).animate({ left:  - $('.carousel-seat').width() * 4 })
+        ++timesRight; 
+    }
+  }
+
+});
+
+  var wrap = document.querySelector('.wrap');
+  
+
+  function resize() { 
+    // var wrap = document.querySelector('.wrap');
+    // if(wrap.clientWidth <= 405){
+      var carousel = document.querySelector('.carousel');
+      carousel.style.left = 0;
+      timesRight = 0;
+    // }
+
+  }
+
+  window.onresize = resize;
 
 
 var $cell = $('.card');
@@ -52,6 +105,11 @@ var $cell = $('.card');
 $cell.find('.js-expander').click(function() {
 
   var $thisCell = $(this).closest('.card');
+
+  var offset = $thisCell.offset();
+  var leftOffset = offset.left - 105;
+  console.log($thisCell.children('.card__expander'))
+  $thisCell.children('.card__expander').css( { 'margin-left': "-" + leftOffset + "px"});
 
   if ($thisCell.hasClass('is-collapsed')) {
     $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
@@ -95,26 +153,6 @@ $grouping.on('click', function(){
 
   console.log($thisCell)
 })
-
-// $cards.find('.js-expander').click(function() {
-
-//   var $thisCell = $(this).closest('.cards');
-
-//   if ($thisCell.hasClass('are-collapsed')) {
-//     $cell.not($thisCell).removeClass('is-expanded').addClass('is-collapsed').addClass('is-inactive');
-//     $thisCell.removeClass('is-collapsed').addClass('is-expanded');
-    
-//     if ($cell.not($thisCell).hasClass('is-inactive')) {
-//       //do nothing
-//     } else {
-//       $cell.not($thisCell).addClass('is-inactive');
-//     }
-
-//   } else {
-//     $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-//     $cell.not($thisCell).removeClass('is-inactive');
-//   }
-// });
 
 
 
